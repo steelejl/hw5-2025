887f2e7b5f5b79a9d598153c9373d1a4f237532f
Jenna Steele <jlsteele@umich.edu>
2025-11-18 17:45:11 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -26,12 +26,58 @@ document.querySelector("#play").addEventListener("click", function(){
 document.querySelector("#pause").addEventListener("click", function(){
 	console.log("this video is paused")
 	video.pause();
-	
-
-
 })
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
 
+
+// slow down the video each time "slower" is clicked
+document.querySelector("#slower").addEventListener("click", function(){
+	console.log("new speed is "+video.playbackRate);
+	video.playbackRate *= 0.90;
+});
+
+//Increase the current video speed each time the button is clicked and log the new speed to the console.
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("new speed is "+video.playbackRate);
+	video.playbackRate /= 0.90;
+});
+
+//skip ahead in the video by 10 seconds each time the button is clicked. If it reaches the end of the video,go back to the start of the video - no farther. Log the current location of the video.	
+document.querySelector("#skip").addEventListener("click", function(){
+	if (video.currentTime + 10 >= video.duration){
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 10;
+	}
+	console.log("current location is "+ video.currentTime + "seconds");
+});
+
+// Mute/unmute the video and update the text in the button.	
+document.querySelector("#mute").addEventListener("click", function(){
+	if (video.muted == false){
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+});
+
+// Change the volume based on the slider and update the volume information.
+	document.querySelector("#slider").addEventListener("input", function(){	
+		video.volume = this.value / 100;
+		console.log("the current volume is "+ video.volume);
+		document.querySelector("#volume").innerHTML = (video.volume * 100) + "%";
+	});
+
+//Utilize the existing oldSchool class on the video element.
+document.querySelector("#vintage").addEventListener("click", function(){
+	video.classList.add("oldSchool");
+});
+
+//Remove the oldSchool class from the video element.
+document.querySelector("#orig").addEventListener("click", function(){
+	video.classList.remove("oldSchool");
+});
